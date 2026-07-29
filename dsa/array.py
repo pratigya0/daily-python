47cd44d12bfac69631d5f1aee9d43f1b26ef8bd6
Pratigya Singh <pratigyasingh085@gmail.com>
2026-07-29 17:52:58 +0530

    first commit

--- dsa/array.py
+++ dsa/array.py
@@ -1,3 +1,13 @@
 from array import *
-vals = array("i",[5, 2.9, 8, 4, 2])
+vals = array("i",[5, 2, 9, 8, 4, 2])
+vals.reverse()   #to reverse
 print(vals)
+
+for i in range(5):     #to print one by one
+    print(vals[i])
+
+newArr = array(vals.typecode, (a for a in vals))   #created new array
+print(newArr.append(9))   # add new number
+
+for e in newArr:
+    print(e)
