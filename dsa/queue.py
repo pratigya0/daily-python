8814e571e23561331f3a8f8259ef451010ddae1d
Pratigya Singh <pratigyasingh085@gmail.com>
2026-08-02 16:39:54 +0530

    update files

--- dsa/queue.py
+++ dsa/queue.py
@@ -1,4 +1,6 @@
-'''class queue:
+# Define a class named Queue
+# A class is like a blueprint for creating queue objects.
+class queue:
     def __init__(self):
         self.values = []
     def enqueue(self, x):
@@ -8,21 +10,30 @@
 
         self.values = self.values[1:]
 
-        return front '''
+        return front 
+
+q1=queue()
+q1.enqueue(10)
+q1.enqueue(20)
+q1.enqueue(30)
+print(q1.values)
+print(q1.dequeue())
+print(q1.values)
 
 
+'''# Create an empty queue (using a list)
+stock_price_queue = []
 
-wmt_stock_price_queue = []
+# Insert stock prices at the front
+stock_price_queue.insert(0,131.10)
+stock_price_queue.insert(0,132.12)
+stock_price_queue.insert(0,135)
 
-wmt_stock_price_queue.insert(0,131.10)
+#Print the queue
+print(stock_price_queue)
 
-wmt_stock_price_queue.insert(0,132.12)
-
-wmt_stock_price_queue.insert(0,135)
-
-print(wmt_stock_price_queue)
-
-removed = wmt_stock_price_queue.pop()
+# Remove the oldest element (FIFO)
+removed = stock_price_queue.pop()
 
 print("Removed:", removed)
-print("Queue after pop:", wmt_stock_price_queue)
+print("Queue after pop:", stock_price_queue)'''
